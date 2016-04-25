6a492b45f6c03d84c63cfb8db0cae8ef962b1a0a
Liia Butler <l173b625@ku.edu>
2016-04-25 15:41:21 -0500

    Add shell to run the pong3d control

---
+++ GameCollection/src/Pong3d1pShell.java
@@ -0,0 +1,22 @@
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.WindowAdapter;
+import com.sun.j3d.utils.applet.MainFrame;
+import com.sun.j3d.utils.universe.*;
+import javax.media.j3d.*;
+import javax.vecmath.*;
+import com.sun.j3d.utils.geometry.Sphere;
+import javax.swing.Timer;
+
+
+public class Pong3d1pShell {
+	public Pong3d1pShell(Pong3d1pGame game){
+		
+		   Pong3d1pControl control= new Pong3d1pControl();
+		   control.addKeyListener(control);
+		   MainFrame display = new MainFrame(control, 700, 500);
+		
+	}
+
+}
