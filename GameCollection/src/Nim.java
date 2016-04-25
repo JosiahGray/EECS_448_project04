3c3b46935ceda747f5c16a0e0516799b88d992fe
Josiah Gray <j305g268@ku.edu>
2016-04-25 15:07:45 -0500

    Add label for buttons. Add prototype for delay system between turns.

--- GameCollection/src/Nim.java
+++ GameCollection/src/Nim.java
@@ -3,6 +3,7 @@ import java.awt.event.*;
 import java.awt.Component;
 import java.awt.GridLayout;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A class that creates a Nim game.
@@ -13,22 +14,23 @@ public class Nim
 {
 	private JPanel gamePanel;
 	private JLabel instrLabel;
-	private JLabel movesLabel;
 	private JLabel stonesLabel;
+	private JLabel movesLabel;
+	private JLabel buttonLabel;
 	private JPanel buttonPanel;
 	private JButton button1;
 	private JButton button2;
 	private JButton button3;
 	private JButton newGameButton;
-	
+
 	private int stones;
 	private boolean gameOver;
 	private boolean isPlayerTurn;
 	private Random rand;
-	
+
 	private String nextLastMove;
 	private String lastMove;
-	
+
 	/**
 	 * The constructor for the Nim class.
 	 * Sets up the game and starts a new game.
@@ -36,14 +38,14 @@ public class Nim
 	public Nim()
 	{
 		rand = new Random();
-		
+
 		//set up game panel
 		setupGame();
-		
+
 		//start new game
 		reset();
 	}
-	
+
 	/**
 	 * A method to get the content panel that holds the game.
 	 * @return JPanel component that holds the game
@@ -52,7 +54,48 @@ public class Nim
 	{
 		return gamePanel;
 	}
-	
+
+	/**
+	 * Sets up the frame the game is played in.
+	 */
+	private void setupGame()
+	{
+		//create and set up GUI components
+		gamePanel = new JPanel(new GridLayout(0,1));
+		buttonPanel = new JPanel(new GridLayout(1,3));
+
+		instrLabel = new JLabel("<html> Welcome to Nim!<br>"
+				+ "You and the computer will take turns<br>"
+				+ "taking between 1-3 stones from the pile.<br>"
+				+ "Whoever takes the last stone loses!</html>");
+		stonesLabel = new JLabel("");
+		movesLabel = new JLabel("");
+		buttonLabel = new JLabel("Pick a number of stones to take:");
+		button1 = new JButton("1");
+		button2 = new JButton("2");
+		button3 = new JButton("3");
+		newGameButton = new JButton("New Game");
+
+		//attach listeners to buttons
+		button1.addActionListener(button1Listener());
+		button2.addActionListener(button2Listener());
+		button3.addActionListener(button3Listener());
+		newGameButton.addActionListener(newGame());
+
+		//add buttons to button panel
+		buttonPanel.add(button1);
+		buttonPanel.add(button2);
+		buttonPanel.add(button3);
+
+		//add components to game panel
+		gamePanel.add(instrLabel);
+		gamePanel.add(stonesLabel);
+		gamePanel.add(movesLabel);
+		gamePanel.add(buttonLabel);
+		gamePanel.add(buttonPanel);
+		gamePanel.add(newGameButton);
+	}
+
 	/**
 	 * Resets the game, returning all variables to their initial state.
 	 */
@@ -63,14 +106,14 @@ public class Nim
 		stones = rand.nextInt(5) + 21;
 		gameOver = false;
 		isPlayerTurn = true;
-		
+
 		nextLastMove = "";
 		lastMove = "";
-		
+
 		stonesLabel.setText(stoneString(stones));
 		updateMoves("You make the first move.");
 	}
-	
+
 	/**
 	 * Updates the moves played.
 	 * The last move is moved to the second to last move
@@ -81,14 +124,14 @@ public class Nim
 	{
 		//set second to last move equal to last move
 		nextLastMove = lastMove;
-		
+
 		//set last move equal to the newMove parameter
 		lastMove = newMove;
-		
+
 		//display updated moves
 		movesLabel.setText("<html>" + nextLastMove + "<br>" + lastMove + "</html>");
 	}
-	
+
 	/**
 	 * Generates and returns a string telling the number of remaining stones
 	 * and an ascii representation of the pile of remaining stones.
@@ -99,23 +142,23 @@ public class Nim
 	{
 		//add number of remaining stones to display string
 		String display = "Remaining stones: " + n + "<br>";
-		
+
 		//generate ascii art representing the pile of remaining stones
 		for(int i = 1; i <= n; i++)
 		{
 			display += "@";
-			
+
 			if((i%5) == 0)
 			{
 				display += "<br>";
 			}
 		}
-		
+
 		//add html tags to allow panel to display multiple lines
 		display = "<html> " + display + " </html>";
 		return display;
 	}
-	
+
 	/**
 	 * Subtracts the number of stones n from the total pile of stones.
 	 * @param n
@@ -124,13 +167,13 @@ public class Nim
 	private boolean takeStones(int n)
 	{
 		stones = stones - n;
-		
+
 		//returns a boolean: true ~ game over; false ~ game is still going
 		if(stones <= 0)
 		{
 			//game over
 			stones = 0;
-			
+
 			//update stone pile
 			stonesLabel.setText(stoneString(stones));
 			return true;
@@ -145,6 +188,24 @@ public class Nim
 	}
 	
 	/**
+	 * Delays the game by the ammount of time passed as a paremeter (in milliseconds)
+	 * @param time
+	 */
+	private void delay(int time)
+	{
+		//delay for the length of time parameter in milliseconds
+		try
+		{
+			TimeUnit.MILLISECONDS.sleep(time);
+			//Thread.sleep(time);
+		}
+		catch (InterruptedException e)
+		{
+			//Handle exception
+		}
+	}
+
+	/**
 	 * Execute player's turn and then follow with the computer's turn if the game is still going.
 	 * @param stonesTaken
 	 */
@@ -156,6 +217,10 @@ public class Nim
 			//player's turn to take stones
 			gameOver = takeStones(stonesTaken);
 			updateMoves("You took " + stonesTaken + " stones.");
+
+			//add a delay between turns
+			//delay(1000);
+
 			//check if player lost this turn
 			if(gameOver)
 			{
@@ -167,12 +232,15 @@ public class Nim
 			{
 				//computer's turn
 				isPlayerTurn = false;
-				
+
 				//computer takes random number of stones (1-3)
 				int compTake = rand.nextInt(3) + 1;
 				gameOver = takeStones(compTake);
 				updateMoves("The computer took " + compTake + " stones.");
-				
+
+				//add a delay between turns
+				//delay(1000);
+
 				//check if computer just lost
 				if(gameOver)
 				{
@@ -188,46 +256,7 @@ public class Nim
 			}
 		}
 	}
-	
-	/**
-	 * Sets up the frame the game is played in.
-	 */
-	private void setupGame()
-	{
-		//create and set up GUI components
-		gamePanel = new JPanel(new GridLayout(0,1));
-		buttonPanel = new JPanel(new GridLayout(1,3));
-		
-		instrLabel = new JLabel("<html> Welcome to Nim!<br>"
-				+ "You and the computer will take turns<br>"
-				+ "taking between 1-3 stones from the pile.<br>"
-				+ "Whoever takes the last stone loses!</html>");
-		stonesLabel = new JLabel("");
-		movesLabel = new JLabel("");
-		button1 = new JButton("1");
-		button2 = new JButton("2");
-		button3 = new JButton("3");
-		newGameButton = new JButton("New Game");
-		
-		//attach listeners to buttons
-		button1.addActionListener(button1Listener());
-		button2.addActionListener(button2Listener());
-		button3.addActionListener(button3Listener());
-		newGameButton.addActionListener(newGame());
-		
-		//add buttons to button panel
-		buttonPanel.add(button1);
-		buttonPanel.add(button2);
-		buttonPanel.add(button3);
-		
-		//add components to game panel
-		gamePanel.add(instrLabel);
-		gamePanel.add(stonesLabel);
-		gamePanel.add(movesLabel);
-		gamePanel.add(buttonPanel);
-		gamePanel.add(newGameButton);
-	}
-	
+
 	/**
 	 * Sets up the action listener for button1
 	 * @return action listener for button1
@@ -241,10 +270,10 @@ public class Nim
 				playerTurn(1);
 			}
 		};
-		
+
 		return listener;
 	}
-	
+
 	/**
 	 * Sets up the action listener for button2
 	 * @return action listener for button2
@@ -258,10 +287,10 @@ public class Nim
 				playerTurn(2);
 			}
 		};
-		
+
 		return listener;
 	}
-	
+
 	/**
 	 * Sets up the action listener for button3
 	 * @return action listener for button3
@@ -275,10 +304,10 @@ public class Nim
 				playerTurn(3);
 			}
 		};
-		
+
 		return listener;
 	}
-	
+
 	/**
 	 * Sets up the action listener for new game button
 	 * @return action listener for new game button
@@ -292,7 +321,7 @@ public class Nim
 				reset();
 			}
 		};
-		
+
 		return listener;
 	}
 }
