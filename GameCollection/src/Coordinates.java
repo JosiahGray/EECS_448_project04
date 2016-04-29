133a3809f16e71545d695335806eef47b1239b74
Liia Butler <l173b625@ku.edu>
2016-04-29 09:19:51 -0500

    Add basic coordinates class to make locating object easier in logic.

---
+++ GameCollection/src/Coordinates.java
@@ -0,0 +1,18 @@
+
+public class Coordinates {
+	float x;
+	float y;
+	float z;
+	public Coordinates(){
+		x = 0;
+		y = 0; 
+		z = 0;
+	
+	}
+	public setCoordinates(float x1, float y1, float z1){
+		x = x1;
+		y = y1;
+		z = z1;
+	}
+
+}
