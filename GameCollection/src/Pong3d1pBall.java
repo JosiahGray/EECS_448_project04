6ee59730744ccc8be033870eb570dbf62e07a977
Liia Butler <l173b625@ku.edu>
2016-05-01 09:22:55 -0500

    Improve ball behavior

--- GameCollection/src/Pong3d1pBall.java
+++ GameCollection/src/Pong3d1pBall.java
@@ -19,42 +19,62 @@ public class Pong3d1pBall{
 	float yLoc;
 	float zLoc;
 	
-	
-	
-	public Pong3d1pBall(Pong3d1pGame theGame){
+	float paddleLength;
+	float paddleWidth;
+	float[] bBounce;
+	int size =5;
+	public Pong3d1pBall(){
 		//get this from Pong Control
-		radius = 0.03f; 
+		radius = 0.025f; 
+		//get this from pong control
+		paddleLength = 0.1f; 
+		paddleWidth = 0.025f;
+		bBounce = new float[size];
+		for(int i=0; i< size; i++){
+			bBounce[i]=1.0f;
+		}
+		bBounce[4] = 0.0f;
 	}
 	
 	//MAKE THIS AN ARRAY SO THAT BALL CAN SQUISH
-	//array = (vector x, vector z, x squish, z squish
-	public void move(Coordinates bCoords, Coordinates hCoords){
-		float[] bBounce = new float[4];
-		for(int i =0; i< 4; i++){
-			bBounce[i] = 0.0f;
-		}
+	//array = (vector x, vector z, x squish, z squish, speed up ball)
+	public float[] move(Coordinates bCoords, Coordinates hCoords, Coordinates cCoords){
+		bBounce[3] = 0.0f;
 		//put in bounds for where ball is located
 		xLoc = bCoords.x;
 		yLoc = bCoords.y;
 		zLoc = bCoords.z;
-		//where all ball extends to
-		float xLeft = xLoc + radius;
-		float xRight = xLoc - radius;
-		//--------Not necessary---------------------
-		float yTop = yLoc + radius;
-		float yBottom = yLoc - radius;
-		//------------------------------------------
-		float zFor = zLoc + radius;
-		float zBack = zLoc - radius;
-		
-		//compare
-		//if it bounces off x bounds
-		if( xLeft < -1.0f ){
+		if((zLoc < (hCoords.z + paddleWidth)) && (zLoc > (hCoords.z)) && xLoc <= (hCoords.x + paddleLength + radius) && xLoc >= (hCoords.x - paddleLength-radius)){// && (xLeft < (hCoords.x + paddleLength)) && (xRight > (hCoords.x)) ){
+			
+			//squish front
+			bBounce[0] = -1.0f;
+			bBounce[1] = -1.0f;
+			bBounce[3] = -0.01f;
+			if(xLoc >= 0.0f){
+				bBounce[4] = (xLoc - hCoords.x + (hCoords.x)/3)/50;
+			} else{
+				bBounce[4] = -(xLoc - hCoords.x + (hCoords.x)/3)/50;
+			}
 			
 		}
-		
-		
-	
+		if(zLoc < (cCoords.z + paddleWidth/2) && (zLoc > cCoords.z) && xLoc <= (cCoords.x + paddleLength + radius) && xLoc >= (cCoords.x - paddleLength)){
+			bBounce[0] = 1.0f;
+			bBounce[1] = 1.0f;
+			bBounce[3] = 0.01f;
+			if(xLoc >= 0.0f){
+				bBounce[4] = (xLoc -cCoords.x + (cCoords.x)/3)/50;
+			} else {
+				bBounce[4] = -(xLoc -cCoords.x + (cCoords.x)/3)/50;
+			}
+		}
+
+		return bBounce;
+	}
+	public void reset(){
+		for(int i=0; i< size; i++){
+			bBounce[i]=1.0f;
+		}
+		bBounce[4] = 0.0f;
 	}
 	
 
