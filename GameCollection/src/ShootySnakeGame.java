3257aaa8122605333dedb07347bd0a5fe9d8e61f
Paul McElroy <Paul McElroy>
2016-05-06 16:40:55 -0500

    Remove console logging.

--- GameCollection/src/ShootySnakeGame.java
+++ GameCollection/src/ShootySnakeGame.java
@@ -617,7 +617,6 @@ class Enemy extends Ball{
     {
       vx = 25*normalX(x0, y0);
       vy = 25*normalY(x0, y0);
-      System.out.println(vx*vx + vy*vy);
       // if(vx*vx + vy*vy > 2000)
       // {
       //   vx = 2000*normalX(0,0);
